6bcb22c04197a7a27188e028c5de39273866d80b
Valexandrin <alex-vadim@mail.ru>
2022-11-13 16:49:25 +0300

    refactoring

--- find_image_duplicates/duplicates_deleter.py
+++ find_image_duplicates/duplicates_deleter.py
@@ -1,15 +1,22 @@
-from duplicates_list_writer import get_path
 from difPy import dif
+from duplicates_list_writer import path_validation
 
 
 def delete_duplicates(folder1: str, folder2: str=None) -> dict:
     dif(folder1, folder2, delete=True, silent_del=True)
-    print('done')
+    print(folder1, folder2, 'done', sep='\n')
 
 
 def main():
-    path = get_path(r'C:\Users\alex-\OneDrive\Изображения\vadim\камера iphone\iCloud Photos')
-    delete_duplicates(path)
+    paths = [
+        r'C:\Users\alex-\OneDrive\Изображения\vadim\камера iphone',
+    ]
+
+    validated_paths = path_validation(paths)
+    if not validated_paths:
+        return
+
+    delete_duplicates(*validated_paths)
 
 
 if __name__ == '__main__':
