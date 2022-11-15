e2c8627d39e6ea7ea71f02ed766bd5936ea70f97
Valexandrin <alex-vadim@mail.ru>
2022-11-15 13:58:25 +0300

    deleter finished

--- find_image_duplicates/duplicates_deleter.py
+++ find_image_duplicates/duplicates_deleter.py
@@ -1,13 +1,16 @@
-from duplicates_size_comparer import get_cells_by_poor_resolution as cells_getter
+import os
+
+from duplicates_list_markup import get_cells_by_poor_resolution as cells_getter
 from openpyxl import load_workbook
 
 
 def delete_files(path_cells: list) -> None:
-    pass
+    for cell in path_cells:
+        os.remove(cell.value)
 
 
 def main():
-    fname = 'find_image_duplicates/duplicates_copy_copy.xlsx'
+    fname = '<anyfile.xlsx>'
     wb = load_workbook(fname)
     ws = wb.active
     cells = cells_getter(ws)
